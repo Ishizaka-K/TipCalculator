8d84717107d3b3e5b912c84c9828e6a17b175fab
Ishizaka-K <kento@i3-systems.com>
2023-06-12 15:59:18 +0900

    add Viewfile

--- TipCalculator/AppDelegate.cs
+++ TipCalculator/AppDelegate.cs
@@ -14,8 +14,14 @@ namespace TipCalculator
         [Export ("application:didFinishLaunchingWithOptions:")]
         public bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
         {
-            // Override point for customization after application launch.
-            // If not required for your application you can safely delete this method
+            // create a new window instance based on the screen size
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            // Assign the root view controller
+            Window.RootViewController = new MyViewController();
+            // make the window visible
+            Window.MakeKeyAndVisible();
+
             return true;
         }
 
